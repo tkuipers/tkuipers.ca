3eef3cdfdae7703fbe49bb89624cb628dad3c6fc
Tyler Kuipers <tkuipers123@gmail.com>
2022-05-22 10:53:57 -0600

    Add main page with simple animation

--- pages/index.vue
+++ pages/index.vue
@@ -11,3 +11,19 @@ export default Vue.extend({
   name: 'IndexPage'
 })
 </script>
+<style>
+body {
+    margin: 0;
+    height: 100vh;
+    font-weight: 100;
+    background: radial-gradient(#a23982,#1f1013);
+    -webkit-overflow-Y: hidden;
+    -moz-overflow-Y: hidden;
+    -o-overflow-Y: hidden;
+    overflow-y: hidden;
+    -webkit-animation: fadeIn 1 1s ease-out;
+    -moz-animation: fadeIn 1 1s ease-out;
+    -o-animation: fadeIn 1 1s ease-out;
+    animation: fadeIn 1 1s ease-out;
+}
+</style>
