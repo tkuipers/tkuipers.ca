baae4e969108da1442426845cfbe27c03cda634e
Tyler Kuipers <tkuipers123@gmail.com>
2022-05-21 18:08:07 -0600

    Commit nuxtjs framework

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <home-page></home-page>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import homePage from '~/components/home-page.vue'
+
+export default Vue.extend({
+  components: { homePage },
+  name: 'IndexPage'
+})
+</script>
