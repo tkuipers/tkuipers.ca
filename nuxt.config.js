860e9882fc7019eca3a6353bd481337862c018d4
Tyler Kuipers <tkuipers123@gmail.com>
2022-05-22 21:54:20 -0600

    Change website homepage

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'website-homepage',
+    title: 'Tyler Kuipers',
     htmlAttrs: {
       lang: 'en'
     },
