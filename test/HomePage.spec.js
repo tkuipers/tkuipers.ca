df277b54061d2990ad1b22a3dbe6dd45e5e43b1b
Tyler Kuipers <tyler.kuipers@ama.ab.ca>
2024-08-10 15:31:03 -0600

    Update the website to use new nuxt

--- test/HomePage.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import HomePage from '@/components/home-page.vue'
-
-describe('HomePage', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(HomePage)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
